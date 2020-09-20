dcbae204c8812499e5bee2e962b57632d967aeb1
Kseniya <pupkoksenia@gmail.com>
2020-09-20 12:14:05 +0300

    without test

--- src/main/java/com/bsu/by/Main.java
+++ src/main/java/com/bsu/by/Main.java
@@ -1,4 +1,60 @@
 package com.bsu.by;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+
+
+
+    public static void newText(ArrayList<String> newList) {
+
+
+        for (int j = 0; j < newList.size(); j++) {
+            StringBuilder tmp = new StringBuilder();
+
+            int i=0;
+            while ( i < newList.get(j).length()) {
+                  if (!(Character.isDigit(newList.get(j).charAt(i)) )) {
+                    tmp.append(newList.get(j).charAt(i));
+
+                }
+                i++;
+            }
+            newList.set(j,tmp.toString());
+        }
+    }
+
+    public static void outText(ArrayList<String> newList) {
+        for (String sentences : newList) {
+            System.out.println(sentences);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+
+            System.out.println("Write strings");
+            ArrayList<String> text = new ArrayList<String>();
+
+            String tmp = "a";
+
+
+            while (!tmp.equals("")) {
+                tmp = input.nextLine();
+                text.add(tmp);
+            }
+
+            newText(text);
+
+            outText(text);
+
+
+
+        } catch (Exception ignored) {
+
+        }
+    }
 }
--- src/test/java/com/bsu/by/MainTest.java
+++ src/test/java/com/bsu/by/MainTest.java
@@ -1,4 +1,11 @@
 package com.bsu.by;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class MainTest {
+
+
+   /* @org.junit.jupiter.api.Test
+    void separate() {
+    }*/
 }
+
